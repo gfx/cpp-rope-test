dcfbd12c0a2e810889e6de1249ffbdfe049640b9
Fuji Goro <fuji.goro@dena.jp>
2011-08-25 12:04:36 +0900

    No shared_ptr

--- rope.cpp
+++ rope.cpp
@@ -4,15 +4,11 @@
 #include <iostream>
 #include <sstream>
 
-#define BOOST_SP_DISABLE_THREADS 1
-#include <boost/shared_ptr.hpp>
-
 #include "./cpp-scoped_timer/scoped_timer.hpp"
 
 namespace gfx {
     class rope {
-        typedef boost::shared_ptr<const std::string> string_ptr;
-        typedef std::deque< string_ptr > list_t;
+        typedef std::deque< std::string > list_t;
         typedef list_t::const_iterator citer;
 
         list_t list_;
@@ -21,8 +17,7 @@ namespace gfx {
         rope() : list_() {
         }
         rope(const char* const cstr) : list_() {
-            string_ptr s(new std::string(cstr));
-            this->list_.push_back(s);
+            this->list_.push_back(std::string(cstr));
         }
         
         rope(const rope& r) : list_(r.list_) { }
@@ -39,7 +34,7 @@ namespace gfx {
             const list_t& l = this->list_;
             const citer end = l.end();
             for(citer i = l.begin(); i != end; ++i) {
-                o << **i;
+                o << *i;
             }
         }
     };
@@ -76,7 +71,7 @@ int main() {
         = "01234567890123456789012345678901234567890123456789"; // 50b
     const char* const shortstr = "."; // 1b
 
-    const int N = 2 * 1000 * 1000;
+    const int N = 10 * 1000 * 1000;
     gfx::scoped_timer_cputime::elapsed_type e;
     for(int i = 0; i < 2; i++){
         gfx::scoped_timer_cputime t(e, "std::string (long)");
