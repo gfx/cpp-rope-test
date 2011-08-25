800db21c57c738db618e17d69233cc9a59fcbf56
Fuji Goro <fuji.goro@dena.jp>
2011-08-25 18:34:56 +0900

    Add strcat.js for node

---
+++ strcat.js
@@ -0,0 +1,32 @@
+#!/usr/local/bin/node
+"use strict";
+
+var lstr = "01234567890123456789012345678901234567890123456789"; // 50 b
+var sstr = "."; // 1 b
+
+var N = 100 * 1000; // number of loop
+var M = 1000;       // number of concatination
+
+function doit(p, name) {
+    var t0 = Date.now();
+    for(var i = 0; i < N; i++) {
+        var s = "";
+        for(var j = 0; j < M; j++) {
+            s += p;
+        }
+    }
+    process.stderr.write(s);
+
+    var t1 = Date.now();
+    console.log(name + ": " + ((t1 - t0) / 1000));
+}
+
+var i;
+for(i = 0; i < 2; i++) {
+    doit(lstr, "long");
+}
+
+for(i = 0; i < 2; i++) {
+    doit(sstr, "short");
+}
+
